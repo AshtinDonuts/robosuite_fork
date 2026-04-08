a9300e6f6f6edaf62f7b86593a5b05d838420bd9
AshtinDonuts <kingwonghku@gmail.com>
2026-04-08 13:28:44 +0930

    added simple running script for simple_test.py

--- mytests/simple_test.py
+++ mytests/simple_test.py
@@ -1,45 +1,19 @@
-# status DEBUG
+import numpy as np
+import robosuite as suite
 
-from robosuite.models import MujocoWorldBase
+# create environment instance
+env = suite.make(
+    env_name="Lift", # try with other tasks like "Stack" and "Door"
+    robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
+    has_renderer=True,
+    has_offscreen_renderer=False,
+    use_camera_obs=False,
+)
 
-world = MujocoWorldBase()
+# reset the environment
+env.reset()
 
-from robosuite.models.robots import Panda
-
-mujoco_robot = Panda()
-
-from robosuite.models.grippers import gripper_factory
-
-gripper = gripper_factory('PandaGripper')
-mujoco_robot.add_gripper(gripper)
-
-
-mujoco_robot.set_base_xpos([0, 0, 0])
-world.merge(mujoco_robot)
-
-from robosuite.models.arenas import TableArena
-
-mujoco_arena = TableArena()
-mujoco_arena.set_origin([0.8, 0, 0])
-world.merge(mujoco_arena)
-
-from robosuite.models.objects import BallObject
-from robosuite.utils.mjcf_utils import new_joint
-
-sphere = BallObject(
-    name="sphere",
-    size=[0.04],
-    rgba=[0, 0.5, 0.5, 1]).get_obj()
-
-sphere.set('pos', '1.0 0 1.0')
-world.worldbody.append(sphere)
-
-
-model = world.get_model(mode="mujoco")
-
-import mujoco
-
-data = mujoco.MjData(model)
-mujoco.mj_forward(model, data)
-while data.time < 1:
-    mujoco.mj_step(model, data)
+for i in range(1000):
+    action = np.random.randn(*env.action_spec[0].shape) * 0.1
+    obs, reward, done, info = env.step(action)  # take action in the environment
+    env.render()  # render on display
